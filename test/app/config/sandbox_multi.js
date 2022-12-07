43e116667af9665dff9f5d85547558779d455721
pnhung177 <pnhung177@gmail.com>
2022-12-07 10:40:06 +0700

    Enhance the general-service and add the example for multiple static dirs config

---
+++ test/app/config/sandbox_multi.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  plugins: {
+    appStaticPages: {
+      entrypoints: [
+        {
+          contextPath: "/multi",
+          publicDir: path.join(__dirname, "../public/case1"),
+          defaultIndex: "index.html",
+        },
+        {
+          contextPath: "/multi",
+          publicDir: path.join(__dirname, "../public/case2"),
+          defaultIndex: "index.html",
+        }
+      ],
+      publicDir: path.join(__dirname, "../public/multi"),
+    },
+    appWebweaver: {
+      defaultRedirectUrl: "/multi"
+    }
+  }
+};
